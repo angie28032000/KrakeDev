e1bb1a1d91798de16edd5356d18500a6142941f7
angie28032000 <angie_mora28@hotmail.com>
2025-09-17 07:26:34 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,4 +1,8 @@
 saludar = function(){
-
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src = "./imagenes/ganar1.gif"
     console.log("ingresar saludar");
+
+
 }
