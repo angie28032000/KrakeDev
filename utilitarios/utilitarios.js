2ab631113f3e2f689f591d829bea17bb11a5670c
angie28032000 <angie_mora28@hotmail.com>
2025-09-17 22:33:20 -0500

    Se separa las funciones al archivo utilitario js

---
+++ utilitarios/utilitarios.js
@@ -0,0 +1,40 @@
+saludar = function(){
+let nombre=recuperarTexto("txtNombre");
+let apellido=recuperarTexto("txtApellido");
+let edad=recuperarInt("txtEdad");
+let estatura=recuperarFloat("txtEstatura");
+let mesajeBienvenida="Bienvenido " + nombre + " " + apellido ;
+mostrarTexto("lblResultado",mesajeBienvenida);
+mostrarImagen("imgSaludo","./imagenes/saludo.gif");
+mostrarTextoEnCaja("txtNombre","");
+
+}
+
+recuperarTexto = function(idComponente){
+   let componete=document.getElementById(idComponente);
+   let valorIngresado=componete.value;
+    return valorIngresado;
+}
+recuperarInt = function(idComponente){
+    let valorCaja =document.getElementById(idComponente).value;
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat = function(idComponente){
+    let valorCaja =document.getElementById(idComponente).value;
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+}
+mostrarTexto = function(idComponente,mensaje){
+    let componete =document.getElementById(idComponente);
+    componete.innerText=mensaje;
+}
+mostrarTextoEnCaja = function(idComponente,mensaje){
+    let componete = document.getElementById(idComponente);
+    componete.value =mensaje;
+
+}
+mostrarImagen = function(idComponente,rutaImagen){
+    let componete = document.getElementById(idComponente);
+    componete.src=rutaImagen;
+}
