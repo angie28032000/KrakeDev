f7954e6e40297ec17e0e9b9dd75f0f59607833ca
angie28032000 <angie_mora28@hotmail.com>
2025-09-18 18:08:15 -0500

    Mostrar valorSubtotal en el componente lblSubtotal

---
+++ puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,17 @@
+calcularValorDescuento = function(monto,porcentajeDescuento){
+    let valorDescuento = monto * porcentajeDescuento /100; 
+    return valorDescuento;
+}
+calcularIVA = function(monto){
+    let valorIva = monto * 0.12 ;
+    return valorIva;
+}
+calcularSubtotal = function(precio,cantidad){
+    let valorTotalProductos = precio * cantidad;
+    return valorTotalProductos;
+}
+calcularTotal = function(subtotal,descuento,iva){
+    let valorTotalPagar = subtotal-descuento +iva;
+    return valorTotalPagar;
+}
+
