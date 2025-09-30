131ccdf22d1eea0e6f09c50685ea00db6cddd9e9
angie28032000 <angie_mora28@hotmail.com>
2025-09-30 16:09:48 -0500

    paso 0 y paso 1 completado

---
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -0,0 +1,33 @@
+
+let palabraSecreta="";
+
+esMayuscula = function (caracter) {
+    if (caracter.length != 1) {
+        return false;
+    }
+    let codigo = caracter.charCodeAt(0);
+    return codigo >= 65 && codigo <= 90;
+
+}
+
+function guardarPalabra() {
+    
+    let palabra = document.getElementById("txtPassword").value.trim();
+    if (palabra.length !== 5) {
+        alert("La palabra debe tener exactamente 5 caracteres.");
+        return;
+    }
+
+    for (let i = 0; i < palabra.length; i++) {
+        if (!esMayuscula(palabra.charAt(i))) {
+            alert(" La palabra debe contener solo letras MAYUSCULAS (A-Z).");
+            return;
+        }
+    }
+
+    palabraSecreta = palabra;
+    console.log("Palabra guardada correctamente:", palabraSecreta);
+}
+
+
+
