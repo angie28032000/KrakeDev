eee0a465c821f5d6cef80f2333a27f9d90a895a9
angie28032000 <angie_mora28@hotmail.com>
2025-09-30 16:56:03 -0500

    terminado AHORCADO

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,5 +1,8 @@
-
 let palabraSecreta = "";
+let intentos = 0;       
+let coincidencias = 0;  
+let errores = 0;        
+
 
 esMayuscula = function (caracter) {
     if (caracter.length != 1) {
@@ -7,12 +10,12 @@ esMayuscula = function (caracter) {
     }
     let codigo = caracter.charCodeAt(0);
     return codigo >= 65 && codigo <= 90;
-
 }
 
-function guardarPalabra() {
 
+guardarPalabra=function() {
     let palabra = document.getElementById("txtPassword").value.trim();
+
     if (palabra.length !== 5) {
         alert("La palabra debe tener exactamente 5 caracteres.");
         return;
@@ -20,52 +23,94 @@ function guardarPalabra() {
 
     for (let i = 0; i < palabra.length; i++) {
         if (!esMayuscula(palabra.charAt(i))) {
-            alert(" La palabra debe contener solo letras MAYUSCULAS (A-Z).");
+            alert("La palabra debe contener solo letras MAYUSCULAS (A-Z).");
             return;
         }
     }
 
     palabraSecreta = palabra;
-    console.log("Palabra guardada correctamente:", palabraSecreta);
+    coincidencias = 0;
+    errores = 0;
+    intentos = 0;
+
+    
+    for (let i = 0; i < 5; i++) {
+        document.getElementById("div" + i).innerText = "";
+    }
+
+    
+    document.getElementById("ahorcadoImagen").src = "";
+
+    console.log(" Palabra guardada correctamente:", palabraSecreta);
 }
+
 
 mostrarLetra = function (letra, posicion) {
     let idDiv = "div" + posicion;
     let div = document.getElementById(idDiv);
     if (!div) {
-        console.error("No existe el div con id:", idDiv);
+        console.error(" No existe el div con id:", idDiv);
         return;
     }
     div.innerText = letra;
-
-
 }
 
-validar = function(letra){
+
+validar = function (letra) {
     let letrasEncontradas = 0;
-    for (let i=0; i>palabraSecreta.length;i++){
-        if (palabraSecreta.charAt(i)===letra){
-            mostrarLetra(letra,i);
+
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        if (palabraSecreta.charAt(i) === letra) {
+            mostrarLetra(letra, i);
             letrasEncontradas++;
         }
     }
-    console.log("letras encontradas", letrasEncontradas);
+
+    if (letrasEncontradas > 0) {
+        coincidencias += letrasEncontradas;
+    } else {
+        errores++;
+        mostrarAhorcado();
+        console.log("La letra no esta en la palabra. Errores:", errores);
+    }
+
+    console.log(" Letras encontradas totales:", coincidencias);
 }
 
-ingresarLetra = function(){
+ingresarLetra = function () {
     let letra = document.getElementById("txtLetra").value.trim();
-    if (letra.length !==1){
-        alert("DEBES INGRESAR UNA SOLA LETRA ");
+
+    if (letra.length !== 1) {
+        alert(" Debes ingresar una sola letra.");
         return;
     }
-    if (!esMayuscula(letra)){
-        alert("SOLO SE ACEPTAN MAYUSCULAS A-Z");
+
+    if (!esMayuscula(letra)) {
+        alert(" SOLO SE ACEPTAN MAYUSCULAS (A-Z).");
         return;
     }
+
+    intentos++;
     validar(letra);
-    document.getElementById("txtLetra").value="";
+
+   
+    if (coincidencias === palabraSecreta.length) {
+        document.getElementById("ahorcadoImagen").src = "./ganador.gif";
+        console.log(" ¡HAS GANADO!");
+    } 
+    
+    else if (intentos >= 10) {
+        document.getElementById("ahorcadoImagen").src = "./gameOver.gif";
+        console.log(" ¡HAS PERDIDO!");
+    }
+
+    document.getElementById("txtLetra").value = "";
 }
 
 
+mostrarAhorcado = function () {
+    let img = document.getElementById("ahorcadoImagen");
+    img.src = "./ahorcado" + errores + ".jpg"; 
+}
 
 
